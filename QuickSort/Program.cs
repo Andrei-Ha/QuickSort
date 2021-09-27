8376250b0880a2739308d01622952c71c2eadc88
Andrei Har <asgoreglyad@gmail.com>
2021-09-27 22:33:01 +0300

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        private static int[] array = new int[] { 5, 9, 3, 7, 6, 1, 2, 4, 8, 0 };
+        static void Main(string[] args)
+        {
+            FPrint();
+            Console.ReadKey();
+        }
+        public static void FQuickSort()
+        {
+            FSort(0, array.Length - 1);
+        }
+        public static void FSort(int left, int right)
+        {
+            if (left != right)
+            {
+
+            }
+        }
+        public static void FPrint()
+        {
+            string res = string.Empty;
+            foreach (int val in array)
+            {
+                res += val + ", ";
+            }
+            res = res[.. ^2];
+            Console.WriteLine(res);
+        }
+    }
+}
